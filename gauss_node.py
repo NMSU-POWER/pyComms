aceddda6da4a71d0411d1b699819bea0d68ae13b
Randall Woodall <randalljwoodall@gmail.com>
2020-05-21 11:21:38 -0600

    Get estimated voltage for angle

--- gauss_node.py
+++ gauss_node.py
@@ -5,6 +5,9 @@
 # Be a node and run individual power flow.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+import numpy as np
+import math
+
 
 class Node:
     def __init__(self, selfV, selfS, neighborV, neighborY):
@@ -19,5 +22,18 @@ class Node:
         S_current = 0
         # Step 1:
         for i in range(len(self.neighborV)):
+            print(self.selfV)
+            print(self.neighborV[i])
+            print(self.neighborY[i])
             S_current -= self.selfV * (self.neighborV[i] * self.neighborY[i])
         print(S_current)
+
+        #Disregard above for now, what about voltages?
+        Yself = -1 * sum(self.neighborY)
+        print(Yself)
+        V_current = np.conj(self.selfS)/self.selfV/Yself
+        sums = 0
+        for v,y in zip(self.neighborV, self.neighborY):
+            sums -= y*v
+        V_current += sums/Yself
+        print(V_current)
