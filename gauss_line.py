04ac98d658561c2dc638aff14942fe80498b29b6
Randall Woodall <randalljwoodall@gmail.com>
2020-05-26 09:20:24 -0600

    Bassis for line agent

---
+++ gauss_line.py
@@ -0,0 +1,13 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Randall Woodall
+# May 2020
+# gauss_line.py
+# Class that acts a the line between two nodes, information passed by this line will connect everything and allow
+# information to slowly spread around the "web".
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Line:
+    def __init__(self):
+        print('I\'m a line!')
+        
