6108119c7fbe518c13686a96d5ef45bbaf0e02a2
Randall Woodall <randalljwoodall@gmail.com>
2020-05-28 11:05:08 -0600

    Functional angle calculation (for 2 bus)

--- gauss_node.py
+++ gauss_node.py
@@ -36,7 +36,9 @@ class Node:
         phasor = (np.conj(self.selfS) / self.selfV - self.selfV * Yself) / self.neighborV[0] / self.neighborY[0]
         print(phasor)
         delta = cmath.phase(phasor) * 360 / 2 / math.pi
+        print(cmath.phase(phasor))
         print(delta)
+        return delta
 
     def gauss_iter(self):
         # Step one, what is the power with the current angles?
