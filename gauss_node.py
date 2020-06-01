3e081eb09b0e018230f0b00ca2332836fa8e4a1b
Randall Woodall <randalljwoodall@gmail.com>
2020-06-01 11:22:15 -0600

    DIscovery of and fixing the mistake

--- gauss_node.py
+++ gauss_node.py
@@ -35,8 +35,9 @@ class Node:
 
     def angle_calculation(self):
         Yself = -1 * sum(self.neighborY)
-        phasor = (np.conj(self.selfS) / self.selfV - self.selfV * Yself) / self.neighborV[0] / self.neighborY[0]
+        phasor = (np.conj(self.selfS) / self.selfV - (self.selfV * Yself)) / self.neighborV[0] / self.neighborY[0]
         print(phasor)
+        print(np.abs(phasor))
         delta = math.degrees(cmath.phase(phasor))
         print(delta)
         return delta
