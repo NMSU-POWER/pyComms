fc028fb1a50fa611b951c234e5b369260292223b
Randall Woodall <randalljwoodall@gmail.com>
2020-06-15 20:54:10 -0600

    Broken, but almost

--- gauss_node.py
+++ gauss_node.py
@@ -13,65 +13,64 @@ import cmath
 # shared_info
 # Informational class only to serve as info sharing medium
 class shared_info:
-    def __init__(self, neighborV, neighborY):
+    def __init__(self, neighborV, neighborY, slack=False):
         self.neighborV = neighborV
         self.neighborY = neighborY
-        self.neighborDelta = [] * len(self.neighborY)
+        self.slack = slack
         # Always start true to kick off Gauss
         self.changed = True
 
 
 class Node:
-    def __init__(self, selfV, selfS, neighborV, neighborY):
-        self.neighborV = neighborV
-        self.neighborDelta = [0] * len(self.neighborV)
-        self.neighborY = neighborY
+    def __init__(self, selfV, selfS, neighbors):
         self.selfV = selfV
-        self.Vmag = selfV
         self.selfS = selfS
+        self.neighbors = neighbors
 
     # node_manager
     # shared_info is a class that holds the shared information to this node
     # Run power flow until we hit a stable state, update if values change anywhere
-    def node_manager(self, shared_info):
-        last_calc = self.gauss_voltage()
+    def node_manager(self):
+        last_calc, _ = self.gauss_voltage()
         # Loop forever, always keep the power flow up to date
         while True:
-            # Only iterate if there has been a change, or if calculated voltage/power doesn't line up with measured
-            if shared_info.changed == True or last_calc[0] != self.selfV or self.power_calc() != self.selfS:
-                print('here')
-        return True
+            self.gauss_voltage()
+            if self.neighbors.slack:
+                self.power_calc()
 
     # gauss_voltage
     # No input argument, Node contains necessary information
     # Calculates own voltage based on current state of voltages and power
     def gauss_voltage(self):
         # Calculate self diagonal admittance
-        Yself = -1 * sum(self.neighborY)
+        Yself = -1 * sum(self.neighbors.neighborY)
         # Normal Gauss voltage method for a node.
         V_current = np.conj(self.selfS / self.selfV)
         sums = 0
-        for v, y, d in zip(self.neighborV, self.neighborY, self.neighborDelta):
-            sums -= y * v * (math.cos(d) + math.sin(d) * 1j)
+        for v, y in zip(self.neighbors.neighborV, self.neighbors.neighborY):
+            sums -= y * v
         V_current += sums
         V_current /= Yself
-        # Send this value back to the main function for distribution
-        return np.abs(V_current), cmath.phase(V_current)
+        # Set own, distribution will automate.
+        self.selfV = V_current
 
     # power_calc
     # No input arguments, Node contains necessary information.
     # Calculates the power at the node with the current nodal voltage information.
     def power_calc(self):
         # Calculate self diagonal admittance
-        Yself = -1 * sum(self.neighborY)
+        Yself = -1 * sum(self.neighbors.neighborY)
         # I = v of each node multiplied by admittance, summation, simplifies where admittance = 0
         I = self.selfV * Yself
-        for v, y, d in zip(self.neighborV, self.neighborY, self.neighborDelta):
-            I += v * (math.cos(d) + 1j*math.sin(d)) * y
+        for v, y in zip(self.neighbors.neighborV, self.neighbors.neighborY):
+            I += v * y
         newS = self.selfV * np.conj(I)
         # New power, based on current voltage information
         self.selfS = newS
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Nothing below tested or proofed with changes (dead code)
+    '''
     def angle_calculation(self):
         Yself = -1 * sum(self.neighborY)
         phasor = (np.conj(self.selfS) / self.selfV - (self.selfV * Yself)) / self.neighborV[0] / self.neighborY[0]
@@ -111,3 +110,4 @@ class Node:
                 sums -= y * v * (math.cos(i) + math.sin(i) * 1j)
             V_current += sums / Yself
             print(V_current)
+    '''
--- parallel_distributed_gauss.py
+++ parallel_distributed_gauss.py
@@ -1,1 +1,66 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Randall Woodall
+# June 2020
+# parallel_distributed_gauss.py
+# Observe the power flow as it's executed by nodes.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from gauss_node import Node
+from gauss_node import shared_info
+import threading
+
+# Set up the shared information for each node
+Node1_share = shared_info(neighborV=[.9826], neighborY=[-46.15+30.77j], slack=True)
+Node2_share = shared_info(neighborV=[1, 1, 1], neighborY=[-46.15+30.77j, -4.615+3.077j, -4.615+3.077j])
+Node3_share = shared_info(neighborV=[.9826], neighborY=[-4.615+3.077j])
+Node4_share = shared_info(neighborV=[.9826], neighborY=[-4.615+3.077j])
+
+# Create the Node objects with shared info object
+Node1 = Node(1, 0, Node1_share)
+Node2 = Node(.9826, 0, Node2_share)
+Node3 = Node(1, -.50, Node3_share)
+Node4 = Node(1, -.50, Node4_share)
+
+# Assign each Node a thread
+thread1 = threading.Thread(target=Node1.node_manager)
+thread2 = threading.Thread(target=Node2.node_manager)
+thread3 = threading.Thread(target=Node3.node_manager)
+thread4 = threading.Thread(target=Node4.node_manager)
+
+# Start up the threads
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+try:
+    while True:
+        # V1 info
+        Node1_share.neighborV = [Node2.selfV]
+
+        # V2 info
+        Node2_share.neighborV = [Node1.selfV, Node3.selfV, Node4.selfV]
+
+        # V3 info
+        Node3_share.neighborV = [Node2.selfV]
+
+        # V4 info
+        Node4_share.neighborV = [Node4.selfV]
+
+        print('Voltages:')
+        print(Node1.selfV)
+        print(Node2.selfV)
+        print(Node3.selfV)
+        print(Node4.selfV)
+        print('Powers:')
+        print(Node1.selfS)
+        print(Node2.selfS)
+        print(Node3.selfS)
+        print(Node4.selfS)
+        print('\n')
+except:
+    # exception out of gauss loop, close threads
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
