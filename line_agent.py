638ed4042dd517aa33bdbd0d413f756757092ca6
Randall Woodall <randalljwoodall@gmail.com>
2020-08-10 14:20:22 -0600

    Comments and simplification

--- line_agent.py
+++ line_agent.py
@@ -7,21 +7,21 @@
 
 from comm_manager_line import Line_comm
 import threading
-import time
 
 if __name__ == '__main__':
-    # Allow both nodes to connect individually
+    # Create and connect to the first node
     node_1 = Line_comm(b'-.44+6.64j')
     node_1.comm_connect(8080)
+    # Create and connect to the second node
     node_2 = Line_comm(b'-.44+6.64j')
     node_2.comm_connect(8081)
 
+    # Node threads need to be started to create a constant spinning communication to both nodes
     threading.Thread(target=node_1.communicate).start()
     threading.Thread(target=node_2.communicate).start()
 
     while True:
-        print(node_1.v)
-        print(node_2.v)
+        # Node 1 needs to have node 2's voltage
         node_1.other_v = node_2.v
+        # Node 2 needs to have node 1's voltage
         node_2.other_v = node_1.v
-        time.sleep(1)
