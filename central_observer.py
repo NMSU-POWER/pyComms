807afd805a7722e7ee15ff9f2ebac8112da3e845
Randall Woodall <randalljwoodall@gmail.com>
2020-08-10 15:57:49 -0600

    basics for central

---
+++ central_observer.py
@@ -0,0 +1,8 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Randall Woodall
+# August 2020
+# central_observer.py
+# Observes the voltage of the distributed nodes.  Provides each node with a list of lines connected.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import socket
