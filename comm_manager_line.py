2a79e01fb25b0a8432f8ffd4bf8dc9ce8f090072
Randall Woodall <randalljwoodall@gmail.com>
2020-08-10 14:34:21 -0600

    the communication interface for the line is commented

--- comm_manager_line.py
+++ comm_manager_line.py
@@ -15,15 +15,24 @@ class Line_comm:
         # other_v is the voltage of the other node connected to the respective line, essentially the voltage the node is
         # communicating to get
         self.other_v = v
+        # The provided admittance will be provided as requested
         self.y = y
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # The connection to be used in the whole object
+        self.conn = None
 
+    #
+    # comm_connect
+    # connect to the first node that sends a connection on the provided port.
     def comm_connect(self, port):
-        self.s.bind(('', port))
-        self.s.listen(1)
-        self.conn, self.addr = self.s.accept()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', port))
+        s.listen(1)
+        self.conn, _ = s.accept()
         print('connected on port ' + str(port))
 
+    #
+    # communicate
+    # Continually send and receive the respective voltage values or the admittance when requested.
     def communicate(self):
         while True:
             val = self.conn.recv(1024)
@@ -31,5 +40,4 @@ class Line_comm:
                 self.conn.sendall(self.y)
             else:
                 self.v = val
-                # print(self.other_v)
                 self.conn.sendall(self.other_v)
