fec49d1896bc2ce3a2652b60e08befc78e6898c4
Randall Woodall <randalljwoodall@gmail.com>
2020-08-12 17:43:21 -0600

    Might actually work, on to debugging

--- central_observer.py
+++ central_observer.py
@@ -6,3 +6,38 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import socket
+import threading
+
+class comm_handle:
+    def __init__(self, connection, addr):
+        self.connection = connection
+        self.addr = addr
+        self.voltage = None
+
+    def communicate(self):
+        while True:
+            self.voltage = self.connection.recv(1024).decode()
+
+
+connection_list = []
+
+
+def accept_connections():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 8080))
+    s.listen()
+    while True:
+        conn, address = s.accept()
+        comm = comm_handle(conn, address)
+        connection_list.append(comm)
+        threading.Thread(target=comm.communicate).start()
+
+
+if __name__ == "__main__":
+    # Accept forever
+    threading.Thread(target=accept_connections).start()
+    # print the values in the comm handles
+    while True:
+        for node in connection_list:
+            print('v @ ' + node.addr + ': ' + node.voltage)
+        print('\n')
