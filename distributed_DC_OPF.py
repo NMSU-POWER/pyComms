5fad8f79258c666d78bc83dc65297f1a020e2735
Randall Woodall <randalljwoodall@gmail.com>
2020-09-29 10:16:40 -0600

    Paralellized so it all runs

--- distributed_DC_OPF.py
+++ distributed_DC_OPF.py
@@ -5,28 +5,34 @@
 # Run a basic DC OPF as a distributed system.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Going to start with the 2 bus case Dr. Ranade presented, some significant looking kinks with more busses.
-# This is not OPF, this is ED
-# There is no consideration of losses in the line, no consideration of the line having impedance.
+import threading
 
 class bus:
-    def __init__(self, pLoad, export, a, b, pGen = 0):
+    def __init__(self, pLoad, export, a, b, pGen=0, id=0,  slack=False):
         # straightforward: this component should change as the algorithm progresses
         self.gen = pGen
-        # This is obvious, if theres a load it goes here
+        # This is obvious, if there's a load it goes here
         self.load = pLoad
-        # This will be gen - load, do we need it?
+        # This will be gen - load, do we need it?  Yes. This must be how we ship out power?
         self.export = export
         # These are generator cost constants
         self.a = a
         self.b = b
-        self.lambda_line = 0
+        self.lambda_line = {}
         self.delta = 0
+        self.slack = slack
+        self.id = id
 
-    def power_calc(self, del_other, ld):
-        self.lambda_line = ld
-        self.gen = (self.lambda_line - self.a) / (2 * self.b)
-        self.delta = (self.gen - self.load) / 1000 + del_other
+    def power_calc(self, del_other, ld, line):
+        self.lambda_line[line] = ld
+        self.gen = (self.lambda_line[line] - self.a) / (2 * self.b)
+        self.export = 0
+        for lines in self.lambda_line:
+            if lines != line:
+                self.export += (self.lambda_line[lines] - self.a) / (2 * self.b)
+        if not self.slack:
+            self.delta = (self.gen - self.load - self.export) / 1000 + del_other
+        print(str(self.id) + ': ' + str(self.gen))
         return self.gen - self.load
 
 
@@ -34,31 +40,34 @@ class line:
     def __init__(self, bus1, bus2):
         self.bus1 = bus1
         self.bus2 = bus2
-        ld_line = 0
-        difference = self.bus1.power_calc(self.bus2.delta, ld_line) + self.bus2.power_calc(self.bus1.delta, ld_line)
+        self.ld_line = 0
+
+    def run(self):
+        difference = self.bus1.power_calc(self.bus2.delta, self.ld_line, self) + \
+                     self.bus2.power_calc(self.bus1.delta, self.ld_line, self)
         epsilon = .001
         while difference < 0 - epsilon or difference > 0 + epsilon:
             if difference < 0:
-                ld_line += .001
+                self.ld_line += .001
             else:
-                ld_line -= .001
-            print(difference)
-            print(ld_line)
-            print(self.bus1.gen)
-            print(self.bus2.gen)
-            print()
-            difference = self.bus1.power_calc(self.bus2.delta, ld_line) + self.bus2.power_calc(self.bus1.delta, ld_line)
+                self.ld_line -= .001
+            difference = self.bus1.power_calc(self.bus2.delta, self.ld_line, self) + \
+                         self.bus2.power_calc(self.bus1.delta, self.ld_line, self)
 
 
 # Essentially a line
 if __name__ == "__main__":
     ld_line = 0
-    bus1 = bus(0, 0, 7.92, .001562 * 2, 0)
-    bus2 = bus(0, 0, 7.85, .00194 * 2, 0)
-    bus3 = bus(300, 0, 7.97, float('inf'), 0)
+    bus1 = bus(0, 0, 7.92, .001562 * 2, 0, 1, True)
+    bus2 = bus(0, 0, 7.85, .00194 * 2, 0, 2, False)
+    bus3 = bus(300, 0, 7.97, float('inf'), 0, 3, False)
 
+    # Duh. These need to be parallel, we're getting stuck in #1
     line1 = line(bus1, bus2)
+    threading.Thread(target=line1.run).start()
     line2 = line(bus2, bus3)
+    threading.Thread(target=line2.run).start()
     line3 = line(bus1, bus3)
+    threading.Thread(target=line3.run).start()
 
     print(line1.bus1.gen)
