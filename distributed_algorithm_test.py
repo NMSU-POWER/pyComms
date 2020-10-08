cf75634c449457cbc442dbc3cfecbd3029a17fab
Randall Woodall <randalljwoodall@gmail.com>
2020-10-08 11:32:58 -0600

    Total export calculation

--- distributed_algorithm_test.py
+++ distributed_algorithm_test.py
@@ -7,18 +7,19 @@
 
 
 # Step 1, calculate the export from every bus given angles
-def export_calc():
-    print('hellow world')
+def export_calc(ang, B):
+    return [100 * B[1][1] * ang[1] + 100 * B[1][2] * ang[2] + 100 * B[1][3] * ang[3],
+            100 * B[2][1] * ang[1] + 100 * B[2][2] * ang[2] + 100 * B[2][3] * ang[3],
+            100 * B[3][1] * ang[1] + 100 * B[3][2] * ang[2] + 100 * B[3][3] * ang[3]]
 
 
-# Step 2.a, Calculate value of export with bus lambda
-def bus_value():
+# Step 2, Calculate value of export with correct lambda
+def bus_value(exported, ld):
     print('export value')
+    
+    for exp, ld_bus in zip(exported, ld):
+        if exp > 0:
 
-
-# Step 2.b, Calculate value of import with other buses lambda
-def other_bus_value():
-    print('import value')
 
 
 # Step 3.a, Adjust lambda upward if price to produce > price received for export, down if else
@@ -43,4 +44,26 @@ def angle_calc():
 
 # The process runner, other functions to be fleshed out as this is built
 if __name__ == '__main__':
-    print('running the main')
+    # DC OPF, all voltages assumed to be 1
+    # Need generation values
+    # Starts at 0
+    gen = [0, 0, 0]
+    # loads as they are
+    load = [0, 0, 100]
+    # Angles start at 0
+    angles = [0, 0, 0]
+    # export starts at 0
+    export = [0, 0, 0]
+    # B matrix
+    B = [[-123.47j, 57.1j, 66.37j], [57.1j, -106.6j, 49.5j],
+     [66.37j, 49.5j, -115.88j]]
+    # Price of import/received for export
+    values = [0, 0, 0]
+    # Lambdas of each bus
+    lds = [0, 0, 0]
+
+    # Start with step 1
+    export = export_calc(angles, B)
+
+    # Step 2
+    value = bus_value(export, lds)
