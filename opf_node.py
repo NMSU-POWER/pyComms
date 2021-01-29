2f2107f5d2763f05faca4c47d5cf46901a31f2de
Randall Woodall <randalljwoodall@gmail.com>
2021-01-29 16:14:59 -0700

    Algorithm fixes
    Runs on single cpu

--- opf_node.py
+++ opf_node.py
@@ -7,6 +7,9 @@
 
 import statistics
 
+# Global for now, constant that affects how quickly a node changes lambda.
+alpha = -.01
+
 
 # Hold information about the line connected to the node objects
 class InternalLine:
@@ -21,6 +24,19 @@ class InternalLine:
         self.lineLambda = 0
         # Power being pushed into the line from this side of a node
         self.powerOut = {}
+        # Lists of other lambdas
+        self.other_lambdas = {}
+
+    def lambda_update(self):
+        collected_lambdas = []
+        power_out = 0
+        for node in self.other_lambdas.keys():
+            collected_lambdas.extend(self.other_lambdas[node])
+            power_out += self.powerOut[node]
+        self.lineLambda = self.lineLambda + power_out * alpha
+        collected_lambdas.append(self.lineLambda)
+        self.lineLambda = statistics.mean(collected_lambdas)
+
 
 # Hold information pertaining to the specific bus (node)
 class Node:
@@ -44,6 +60,7 @@ class Node:
             line.delta[self] = self.delta
             line.powerOut[self] = 0
 
+    # All the node's calculations can happen in one shot
     def update_power_angle(self):
         # Update power first
         self.power = (statistics.mean([x.lineLambda for x in self.lines]) - self.a) / self.b
@@ -54,15 +71,21 @@ class Node:
             for node in line.delta.keys():
                 if node != self:
                     otherDelta = line.delta[node]
-            delta_reactance.append((otherDelta, line.reactance))
+                    delta_reactance.append((otherDelta, line.reactance))
         self.delta = (self.power - self.load + sum([x[0] / x[1] for x in delta_reactance]))/self.reactanceSum
         # Now update the power transferred out on this side of the line object
         for line in self.lines:
-            otherDelta = {}
+            line.delta[self] = self.delta
+            otherDelta = 0
             for node in line.delta.keys():
                 if node != self:
                     otherDelta = line.delta[node]
             line.powerOut[self] = (self.delta - otherDelta) / line.reactance
+            lambdas = []
+            for line_for_lambda in self.lines:
+                if line_for_lambda != line:
+                    lambdas.append(line_for_lambda.lineLambda)
+            line.other_lambdas[self] = lambdas
 
 
 if __name__ == '__main__':
@@ -76,8 +99,23 @@ if __name__ == '__main__':
     node3 = Node(load=400, a=1, b=.02, lines=[line2, line3])
 
     # Testing
-    node1.update_power_angle()
-    print(node1.power)
-    print(node1.delta)
-    print(line1.powerOut)
-    print(line2.powerOut)
+    for i in range(100):
+        node1.update_power_angle()
+        node2.update_power_angle()
+        node3.update_power_angle()
+        line1.lambda_update()
+        line2.lambda_update()
+        line3.lambda_update()
+
+        print('iteration: ' + str(i + 1))
+
+        print(node1.power)
+        print(node2.power)
+        print(node3.power)
+
+        print(line1.powerOut)
+        print(line2.powerOut)
+        print(line3.powerOut)
+        print(line1.lineLambda)
+        print(line2.lineLambda)
+        print(line3.lineLambda)
