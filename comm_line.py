7ca7b3ad0ecfcdf8b0192e213b4c55c18a60ff20
Randall Woodall <randalljwoodall@gmail.com>
2021-02-03 15:17:51 -0700

    Basic connection

--- comm_line.py
+++ comm_line.py
@@ -6,3 +6,20 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import socket
+
+class LineConnection:
+    def __init__(self):
+        self.connection = None
+        self.connect()
+
+    def connect(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', 8080))
+        s.listen(0)
+        self.connection, addr = s.accept()
+        print(addr)
+        print(self.connection.recv(1024))
+
+
+if __name__ == "__main__":
+    line = LineConnection()
--- comm_node.py
+++ comm_node.py
@@ -7,5 +7,17 @@
 
 import socket
 
+class NodeConnection:
+    def __init__(self, ip):
+        self.ip = ip
+        self.connection = None
+        self.connect()
+
+    def connect(self):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((self.ip, 8080))
+        self.connection.sendall(b'{test: 5}')
 
 
+if __name__ == "__main__":
+    connection = NodeConnection('10.0.0.10')
