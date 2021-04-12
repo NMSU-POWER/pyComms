2c4525b79802ab7375eb83c54959176d7bf3225e
Randall Woodall <randalljwoodall@gmail.com>
2021-04-12 13:13:17 -0600

    shadow prices

--- gurobi_check.py
+++ gurobi_check.py
@@ -28,15 +28,15 @@ model.addConstr(flows[1] - 100 * B[1] * (angles[2] - angles[0]) == 0)
 model.addConstr(flows[2] - 100 * B[2] * (angles[2] - angles[1]) == 0)
 
 # Load Constraints
-model.addConstr(power[0] - flows[0] - flows[1] == 0)
-model.addConstr(power[1] + flows[0] - flows[2] == 0)
-model.addConstr(power[2] + flows[1] + flows[2] == load)
+cost1 = model.addConstr(power[0] - flows[0] - flows[1] == 0)
+cost2 = model.addConstr(power[1] + flows[0] - flows[2] == 0)
+cost3 = model.addConstr(power[2] + flows[1] + flows[2] == load)
 
 # Hold delta bus 1 at 0
 model.addConstr(angles[0] == 0)
 
 model.setObjective(power[0] * power[0] * .005 + power[0] + power[1] * power[1] * .0075 + power[1] + power[2] * power[2]
-      * .01 + power[2], GRB.MINIMIZE)
+                   * .01 + power[2], GRB.MINIMIZE)
 
 model.optimize()
 
@@ -45,3 +45,9 @@ print(power)
 print(flows)
 
 print(angles)
+
+print(cost1.Pi)
+
+print(cost2.Pi)
+
+print(cost3.Pi)
