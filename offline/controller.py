b79b3e4477bf39dbcbc91fdd6bfeb772bb8234f9
Randall Woodall <randalljwoodall@gmail.com>
2021-05-25 16:23:23 -0600

    Generate nodes/lines from xlsx

--- offline/controller.py
+++ offline/controller.py
@@ -8,6 +8,7 @@
 from statistics import mean
 from offline.line import Line
 from offline.node import Node
+import pandas as pd
 
 # pseudocode:
 # For every input line
@@ -28,6 +29,51 @@ from offline.node import Node
 
 # Standard setup for 3-bus (testing)
 if __name__ == "__main__":
+    branches = pd.read_excel('24_bus.xlsx', sheet_name='Branch Information')
+    gens = pd.read_excel('24_bus.xlsx', sheet_name='Generation Information').sort_values(by='Bus ID').\
+        reset_index(drop=True)
+    # exit()
+
+    # Set up the line devices
+    lines = []
+    max_bus = 0
+    for i in range(len(branches['Susceptance'])):
+        line = Line(branches['Susceptance'][i] * 1j, branches['From Bus'][i], branches['To Bus'][i], i)
+        lines.append(line)
+        if line.conn2 > max_bus:
+            max_bus = line.conn2
+        if line.conn1 > max_bus:
+            max_bus = line.conn1
+        print('Line ' + str(line.device_id) + ' from ' + str(line.conn1) + ' to ' + str(line.conn2) + ' with x=' +
+              str(line.x))
+    print('Number of buses: ' + str(max_bus))
+
+    # Set up the node devices, pull # nodes from lines, then make generator information from generators? How to combine
+    # generators?  Keeping it super simple right now.  Simple average, do unique generator objects later.
+    j = 0
+    nodes = []
+    for i in range(1, max_bus + 1):
+        gen_vals = []
+        while j < len(gens['Bus ID']) and i == gens['Bus ID'][j]:
+            gen_vals.append(gens['Incremental Cost in $/MWh'][j])
+            j += 1
+        # !! Every node has 100MW load
+        try:
+            gen_cost = mean(gen_vals)
+        except Exception as e:
+            print('At bus ' + str(i) + ' no generation units.')
+            print(e)
+            gen_cost = float('inf')
+        # We need all the lines connected to this node.
+        connections = []
+        for j in range(len(branches['From Bus'])):
+            if branches['From Bus'][j] == i or branches['To Bus'][j] == i:
+                connections.append(lines[j])
+        node = Node(0, gen_cost, 100, len(lines) + i, connections)
+        print('Node ' + str(i) + ' with id ' + str(node.device_id) + ' with generation IC=' + str(node.b))
+
+
+    '''
     line1 = Line(57.15j, 1)
     line2 = Line(66.66j, 2)
     line3 = Line(50j, 3)
@@ -35,6 +81,9 @@ if __name__ == "__main__":
     node1 = Node(1, .01, 0, 4, [line1, line2])
     node2 = Node(1, .015, 0, 5, [line1, line3])
     node3 = Node(1, .02, 400, 6, [line2, line3])
+    
+
+    exit()
 
     while(True):
         node1.update_p()
@@ -53,3 +102,4 @@ if __name__ == "__main__":
         print(mean([line1.ld, line3.ld]))
         print(mean([line2.ld, line3.ld]))
         print()
+    '''
--- offline/line.py
+++ offline/line.py
@@ -8,15 +8,16 @@
 from statistics import mean
 
 class Line:
-    def __init__(self, y, id_input=-1):
+    def __init__(self, y, conn1, conn2, id_input=-1):
         self.y = y
         self.x = (1 / self.y).imag
+        self.conn1 = conn1
+        self.conn2 = conn2
         self.device_id = id_input
         self.ld = 0
         self.alpha = -.001
         self.delta = {}
         self.ptie = {}
-        print("line " + str(self.device_id) + " setup.")
 
     def update_lambda(self, other_ld):
         self.ld = self.ld + self.alpha * sum([self.ptie[x] for x in self.ptie.keys()])
