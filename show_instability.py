64884febea1c1e67f8497aedced60c4023735101
Randall Woodall <randalljwoodall@gmail.com>
2021-06-02 12:36:31 -0600

    Show the first 100 iterations

--- show_instability.py
+++ show_instability.py
@@ -12,12 +12,19 @@ P1 = -100
 P2 = -450
 alpha = -.01
 
-iterations = range(1, 201)
+iterations = range(1, 101)
 
 result = [lambda_start]
 for iteration in iterations[:99]:
     result.append(result[iteration - 1] + (P1 + P2) * alpha)
 
+plt.plot(iterations, result)
+plt.title('System power value versus iterations without P1, P2 change')
+plt.xlabel('Iteration')
+plt.ylabel('System power value')
+plt.show()
+
+iterations = range(1, 201)
 P1 = 49900
 P2 = 24550
 
